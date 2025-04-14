afe2f3648e1c8965160933e975c6578ac7dc1933
IsaquePerezRodrigues <isaquenovo127@gmail.com>
2025-04-14 17:54:34 -0300

    featch: methodes gets/gets_id

--- index.js
+++ index.js
@@ -15,10 +15,38 @@ app.use(express.json());
  
  
  
-// Rota GET para todos os usuários
+// Rota GET para todos os produtos
 app.get("/produtos", async (req, res) => {
     try {
         const consulta = await axios.get('http://localhost:3000/produtos');
+        console.log("Consulta realizada");
+        return res.json(consulta.data);
+    } catch (error) {
+        console.error("Erro na consulta:", error);
+        return res.status(500).json({ error: "Erro ao consultar produtos" });
+    }
+});
+ 
+ 
+// Rota GET por ID
+app.get("/produtos/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:3000/produtos/${id}`);
+        console.log(`Consulta por ID ${id} realizada`);
+        return res.json(response.data);
+    } catch (error) {
+        console.error(`Erro ao consultar usuarios com ID ${id}:`, error);
+        if (error.response?.status === 404) {
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        }
+        return res.status(500).json({ error: "Erro ao consultar produtos" });
+    }
+});
+
+app.get("/usuarios", async (req, res) => {
+    try {
+        const consulta = await axios.get('http://localhost:3000/usuarios');
         console.log("Consulta realizada");
         return res.json(consulta.data);
     } catch (error) {
@@ -41,6 +69,118 @@ app.get("/produtos/:id", async (req, res) => {
             return res.status(404).json({ error: "Usuário não encontrado" });
         }
         return res.status(500).json({ error: "Erro ao consultar usuário" });
+    }
+});
+
+app.get("/categoria", async (req, res) => {
+    try {
+        const consulta = await axios.get('http://localhost:3000/categoria');
+        console.log("Consulta realizada");
+        return res.json(consulta.data);
+    } catch (error) {
+        console.error("Erro na consulta:", error);
+        return res.status(500).json({ error: "Erro ao consultar categoria" });
+    }
+});
+ 
+ 
+// Rota GET por ID
+app.get("/categoria/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:3000/categoria/${id}`);
+        console.log(`Consulta por ID ${id} realizada`);
+        return res.json(response.data);
+    } catch (error) {
+        console.error(`Erro ao consultar categoria com ID ${id}:`, error);
+        if (error.response?.status === 404) {
+            return res.status(404).json({ error: "Categoria não encontrado" });
+        }
+        return res.status(500).json({ error: "Erro ao consultar categoria" });
+    }
+});
+
+app.get("/endereco", async (req, res) => {
+    try {
+        const consulta = await axios.get('http://localhost:3000/endereco');
+        console.log("Consulta realizada");
+        return res.json(consulta.data);
+    } catch (error) {
+        console.error("Erro na consulta:", error);
+        return res.status(500).json({ error: "Erro ao consultar endereco" });
+    }
+});
+ 
+ 
+// Rota GET por ID
+app.get("/endereco/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:3000/endereco/${id}`);
+        console.log(`Consulta por ID ${id} realizada`);
+        return res.json(response.data);
+    } catch (error) {
+        console.error(`Erro ao consultar endereco com ID ${id}:`, error);
+        if (error.response?.status === 404) {
+            return res.status(404).json({ error: "endereco não encontrado" });
+        }
+        return res.status(500).json({ error: "Erro ao consultar endereco" });
+    }
+});
+
+app.get("/pedido", async (req, res) => {
+    try {
+        const consulta = await axios.get('http://localhost:3000/pedido');
+        console.log("Consulta realizada");
+        return res.json(consulta.data);
+    } catch (error) {
+        console.error("Erro na consulta:", error);
+        return res.status(500).json({ error: "Erro ao consultar pedido" });
+    }
+});
+ 
+ 
+// Rota GET por ID
+app.get("/pedido/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:3000/pedido/${id}`);
+        console.log(`Consulta por ID ${id} realizada`);
+        return res.json(response.data);
+    } catch (error) {
+        console.error(`Erro ao consultar pedido com ID ${id}:`, error);
+        if (error.response?.status === 404) {
+            return res.status(404).json({ error: "pedido não encontrado" });
+        }
+        return res.status(500).json({ error: "Erro ao consultar pedido" });
+    }
+});
+
+app.get("/pedido_itens", async (req, res) => {
+    try {
+        const consulta = await axios.get('http://localhost:3000/pedido_itens');
+        console.log("Consulta realizada");
+        return res.json(consulta.data);
+    } catch (error) {
+        console.error("Erro na consulta:", error);
+        return res.status(500).json({ error: "Erro ao consultar pedido_itens" });
+    }
+});
+ 
+ 
+// Rota GET por ID
+app.get("/pedido_itens/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:3000/pedido_itens/${id}`);
+        console.log(`Consulta por ID ${id} realizada`);
+        return res.json(response.data);
+    } catch (error) {
+        console.error(`Erro ao consultar pedido_itens com ID ${id}:`, error);
+        if (error.response?.status === 404) {
+            return res.status(404).json({ error: "pedido_itens não encontrado" });
+        }
+        return res.status(500).json({ error: "Erro ao consultar pedido_itens" });
     }
 });
  
